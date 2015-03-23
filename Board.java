52d7d3c50115fcc2b983fa9b1f98d41404f83cfd
igrief15 <igrief15@punahou.edu>
2015-03-23 11:28:17 -1000

    ok
    turns out you need to put () after the methods

--- Board.java
+++ Board.java
@@ -23,27 +23,36 @@ public class Board
     return map.get(index);
   }
   
-  public void drawBoard(int numRows, int numCols)
+  
+  
+  public void drawBoard(int numRows, int numCols) //ok so it needs to create a few rows of strings concatenized together
   {
-    double[][] mapArray = new double[numRows][numCols]; //view is a 2D array
+    for (int i = 0; i < numRows; i++)
+    {
+      
+    }
+    
+    
+    String[][] mapArray = new String[numRows][numCols]; //view is a 2D array
     int mapIndex = 0;//index of the 1D array
     
     for(int row = 0; row < numRows; row++)//iterates through all rows and columns
     {
-      for(int col = 0; col < numCols; col++)
-      {
-        if(row % 2 == 0) //if the row index is even (starting with the first one)
-          mapArray[row][col] = map.indexOf(5); //ok
-        else //if row is odd
-          mapArray[row][numCols - col - 1] = map.indexOf(2); //do it backwards
-        
+      for(int col = 0; col < numCols; col++){
+        mapArray[row][col] = map.get(mapIndex).getDisplay();
         mapIndex++;
       }
     }
-    
-    
-    
-  }
-  
-  
+  } 
 }
+
+/*
+ * {
+ if(row % 2 == 0) //if the row index is even (starting with the first one)
+ mapArray[row][col] = map.indexOf(5); //how do i even get an element at a certain index, i forgot
+ else //if row is odd
+ mapArray[row][numCols - col - 1] = map.indexOf(2); //do it backwards
+ 
+ mapIndex++;
+ }
+ */
