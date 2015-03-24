1d1468564e2840500279f7acfe6eaa31a1a36b56
igrief15 <igrief15@punahou.edu>
2015-03-23 16:56:48 -1000

    ok p2
    so now the display variable is concatenated with the type prefix, which
    is now called function

--- Position.java
+++ Position.java
@@ -3,34 +3,36 @@ public class Position
 {
   int index; //position in the board array, not the value it represents
   String display; //a string of the value represented on the board
-  int type; //type of board position (subtract or add coin, or play game)
+  String type; //type of board position (subtract or add coin, or play game)
+  int function;
   
   public Position() //default constructor
   {
     index = 0;
     display = "1";
-    type = 0;
+    function = 0;
   }
   public Position(int ind, String dis, int typ) //constructor with all 3 parameters for instance variables
   {
     index = ind;
     display = dis;
+    function = typ;
     
     if (typ < 2) //random dice roll for position type
     {
-      type = 0;
+      display = "" + display;
     }
     else if (typ < 4)
     {
-      type = 1;
+      display = "+" + display;
     }
     else if (typ <6)
     {
-      type = 2;
+      display = "-" + display;
     }
     else
     {
-      type = 3;
+      display = "[" + display;
     }
   }
   
@@ -38,8 +40,24 @@ public class Position
     display = dis;
   }
   
-  public int getType(){ //returns the block type of whatever space it called
+  public String getType(){ //returns the block type of whatever space it called
     return type;
+  }
+  
+  public int getFunction(){//should return either a coin value or call a game
+    if(function == 0){
+      return 0;
+    }
+    else if(function == 1){
+      return 3;
+    }
+    else if(function == 2){
+      return -3; 
+    }
+    else{ 
+      return 0;
+    }
+    
   }
   
   public String getDisplay(){ //returns whatever it is that is displayed at the given position (map starts on 1)
